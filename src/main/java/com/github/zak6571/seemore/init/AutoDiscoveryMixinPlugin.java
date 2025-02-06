c949e0beae3fce53e30a147826b2bce5b3eb7a16
GitHub Action <action@github.com>
2025-02-06 11:13:50 +0000

    Initialize template repository proper
    Set package name to com.github.zak6571.seemore
    Set project name to SeeMore
    Set modid to seemore

--- src/main/java/com/github/zak6571/seemore/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/zak6571/seemore/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.zak6571.seemore.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/zak6571/seemore/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/zak6571/seemore/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.zak6571.seemore.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
